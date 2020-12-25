0821dba9fc364af23837fbac3ab0a9c281815f80
opuszczone <76592467+opuszczone@users.noreply.github.com>
2020-12-25 17:48:07 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSQZtCBCMlkhf5PfG7MrH_WmUVb9J8hIryofAnRP2PV28UGEWU843JwrPMq59n4Vmy7RKf0IYFd-byH/pubhtml';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1NzHNcxXdZIY_yuEnFBpXA8bYVQo4H9kxAOJD4u1BIY4/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
